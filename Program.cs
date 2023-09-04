b5decc0221a58cb99e481ab9210c12171b6ecf9a
BseDuarte <akerathromerin@gmail.com>
2023-09-03 20:42:58 -0400

    Adicionando arquivos da pasta X

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+ 
+            Console.WriteLine("Você digitou: " + v1);
+
+            if (v1 >= 5)
+            { Console.WriteLine("Excelente!!!");
+                
+        }else
+            {
+                Console.WriteLine("Só isso?");
+            }
+        }
+    }
+}
